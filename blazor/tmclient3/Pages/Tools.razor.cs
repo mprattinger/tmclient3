3ba0304587ada62f3c3da0366203c01912393d1c
Michael Prattinger <mprattinger@outlook.com>
2020-02-16 16:36:08 +0100

    added blazor

---
+++ blazor/tmclient3/Pages/Tools.razor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazor.Pages
+{
+    public partial class Tools
+    {
+        async Task beep()
+        {
+            try
+            {
+                var ctrl = new GpioController();
+                ctrl.OpenPin(5, PinMode.Output);
+                ctrl.Write(5, PinValue.High);
+                await Task.Delay(1000);
+                ctrl.Write(5, PinValue.Low);
+            }
+            catch (System.NotSupportedException ex)
+            {
+                Console.WriteLine("Would toggle pin 5 if it exists!");
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error toogling buzzer!", ex);
+            }
+        }
+    }
+}
